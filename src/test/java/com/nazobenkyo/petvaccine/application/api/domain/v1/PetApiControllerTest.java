540d66bf3b04a80ecd0fb0d97e802ee8496636af
ervitis <vitomarti@gmail.com>
2021-06-12 21:36:58 +0200

    chore: enhace tests

--- src/test/java/com/nazobenkyo/petvaccine/application/api/domain/v1/PetApiControllerTest.java
+++ src/test/java/com/nazobenkyo/petvaccine/application/api/domain/v1/PetApiControllerTest.java
@@ -1,15 +1,20 @@
 package com.nazobenkyo.petvaccine.application.api.domain.v1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nazobenkyo.petvaccine.application.api.domain.v1.model.PetCreate;
 import com.nazobenkyo.petvaccine.application.api.security.domain.privileges.PrivilegeConstants;
 import com.nazobenkyo.petvaccine.application.api.security.domain.roles.RoleConstants;
 import com.nazobenkyo.petvaccine.domain.mapper.ToByteArray;
+import com.nazobenkyo.petvaccine.domain.repository.IPetRepository;
+import com.nazobenkyo.petvaccine.model.Pet;
 import com.nazobenkyo.petvaccine.security.generator.JWTAuth0Token;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -21,7 +26,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Collections;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +38,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(properties = "app.scheduling.enable=false")
 @WebAppConfiguration
 @SpringBootTest
+@WithUserDetails("doctor@petvaccine.com")
+@WithMockUser(username = "doctor@petvaccine.com", roles = {"ROLE_DOCTOR"}, password = "123456",
+        authorities = {"CREATE_PETS_PRIVILEGE", "UPDATE_PETS_PRIVILEGE", "DELETE_PETS_PRIVILEGE", "GET_PETS_PRIVILEGE"})
 public class PetApiControllerTest {
 
     @Autowired
@@ -37,12 +48,19 @@ public class PetApiControllerTest {
 
     private final JWTAuth0Token jwtAuth0Token = new JWTAuth0Token();
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @MockBean
+    private IPetRepository repository;
+
     @Test
-    @WithUserDetails("doctor@petvaccine.com")
-    @WithMockUser(username = "doctor@petvaccine.com", roles = {"ROLE_DOCTOR"}, password = "123456",
-            authorities = {"CREATE_PETS_PRIVILEGE", "UPDATE_PETS_PRIVILEGE", "DELETE_PETS_PRIVILEGE", "GET_PETS_PRIVILEGE"})
     public void whenCreatePet_thenReturnsPetData() throws Exception {
         PetCreate body = PetCreate.builder().name("doggy").ownerEmail("owner@test.com").type("Golden").build();
+        Pet pet = Pet.builder().name(body.getName()).ownerEmail(body.getOwnerEmail()).type(body.getType()).id("123456").build();
+
+        when(this.repository.findByOwnerEmail(any())).thenReturn(Optional.empty());
+        when(this.repository.save(any())).thenReturn(pet);
+
         MvcResult result = mockMvc.perform(
                 post("/v1/pet")
                         .accept(MediaType.APPLICATION_JSON)
@@ -52,6 +70,8 @@ public class PetApiControllerTest {
         )
                 .andExpect(status().isCreated())
                 .andReturn();
-        System.out.println(result.getResponse().getContentAsString());
+        body = mapper.readValue(result.getResponse().getContentAsString(), PetCreate.class);
+        Assertions.assertNotNull(body);
+        Assertions.assertEquals("123456", body.getId());
     }
 }
